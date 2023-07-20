9c69bb648c695dfae0351132d66e36cebe15ba70
yehonatan yehezkel <yoni333@gmail.com>
2023-07-20 11:13:56 +0300

    add mongo db project

--- mongoDB/Program.cs
+++ mongoDB/Program.cs
@@ -1,7 +1,12 @@
+using BookStoreApi.Models;
+using BookStoreApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+builder.Services.AddSingleton<BooksService>();
 
 
 builder.Services.AddControllers().AddJsonOptions(
